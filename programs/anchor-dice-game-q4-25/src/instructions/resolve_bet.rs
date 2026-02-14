d96e4c448328e5dc62661294441ab1ed0104e279
AdemolaSam <oduntanade2721@gmail.com>
2026-02-14 21:56:07 +0100

    First commit with tests

---
+++ programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
@@ -0,0 +1,118 @@
+use crate::{errors::DiceError, Bet};
+use anchor_instruction_sysvar::{Ed25519InstructionSignatures};
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
+use solana_program::{
+    ed25519_program, hash::hash, sysvar::instructions::load_instruction_at_checked
+};
+
+#[constant]
+const HOUSE_FEE: u64 = 150; // basis point -> 1.5%
+
+#[derive(Accounts)]
+pub struct ResolveBet<'info> {
+    #[account(mut)]
+    pub house: Signer<'info>,
+    ///CHECK: this is safe
+    #[account(mut)]
+    pub player: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", house.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
+       #[account(
+        mut,
+        has_one = player,
+        close = player,
+        seeds = [b"bet", vault.key().as_ref(), bet.seed.to_le_bytes().as_ref()],
+        bump = bet.bump
+    )]
+    pub bet: Account<'info, Bet>,
+    ///CHECK: this is safe
+    #[account(
+        address = solana_program::sysvar::instructions::ID
+    )]
+    pub instruction_sysvar: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>
+}
+
+
+impl<'info> ResolveBet<'info> {
+
+    pub fn verify_ed25519_signature(&self, sig: &[u8]) -> Result<()> {
+       
+        let ix = load_instruction_at_checked(0, &self.instruction_sysvar.to_account_info()).map_err(|_| DiceError::Ed25519Program)?;
+
+        //check if instruction's program Id is from ed25519 program
+        require_eq!(ix.program_id, ed25519_program::ID, DiceError::Ed25519Program);
+
+        //ensure no accounts in instruction
+        require_eq!(ix.accounts.len(), 0, DiceError::Ed25519Accounts);
+
+
+        let signatures = Ed25519InstructionSignatures::unpack(&ix.data).map_err(|_| DiceError::Ed25519Signature)?.0;
+
+        //check numbers of signatures
+        require_eq!(signatures.len(), 1, DiceError::Ed25519Signature);
+
+        let signature = &signatures[0];
+
+        // ensure the signature is verifiable
+        require!(signature.is_verifiable, DiceError::Ed25519Header);
+
+        require_keys_eq!(signature.public_key.ok_or(DiceError::Ed25519Pubkey)?, self.house.key());
+
+        require!(&signature.signature.ok_or(DiceError::Ed25519Signature)?.eq(sig), DiceError::Ed25519Signature);
+
+        require!(&signature.message.as_ref().ok_or(DiceError::Ed25519Message)?.eq(&self.bet.to_slice()), DiceError::Ed25519Message);
+
+
+        Ok(())
+    }
+
+    pub fn resolve_bet(&self, bumps: &ResolveBetBumps, sig: &[u8]) -> Result<()> {
+        let hash = hash(sig).to_bytes();
+        let mut hash_16: [u8; 16] = [0; 16];
+        hash_16.copy_from_slice(&hash[0..16]);
+        let lower: u128 = u128::from_le_bytes(hash_16);
+        
+        hash_16.copy_from_slice(&hash[16..32]);
+        let upper = u128::from_le_bytes(hash_16);
+
+        let roll = lower.wrapping_add(upper).wrapping_rem(100) as u8 + 1;
+        
+
+        if self.bet.roll > roll {
+            let payout = (self.bet.amount as u128)
+                .checked_mul(10000 - HOUSE_FEE as u128).ok_or(DiceError::Overflow)?
+                // .checked_div(self.bet.roll as u128 -1).ok_or(DiceError::Overflow)?
+                .checked_div(100).ok_or(DiceError::Overflow)?;
+
+            let signer_seeds : &[&[&[u8]]] = &[
+                &[
+                    b"vault",
+                    &self.house.key().to_bytes(),
+                    &[bumps.vault]
+                ]
+            ];
+
+            let accounts = Transfer {
+                from: self.vault.to_account_info(),
+                to: self.player.to_account_info()
+            };
+
+            let cpi_ctx = CpiContext::new_with_signer(
+                self.system_program.to_account_info(), 
+                accounts,
+                signer_seeds
+            );
+            transfer(cpi_ctx, payout as u64)?;
+        }
+        Ok(())
+    }
+
+}
--- tests/anchor-dice-game-q4-25.ts
+++ tests/anchor-dice-game-q4-25.ts
@@ -1,16 +1,128 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { AnchorDiceGameQ425 } from "../target/types/anchor_dice_game_q4_25";
+import { AnchorDice2026 } from "../target/types/anchor_dice_2026";
+import {
+  Ed25519Program,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  SYSVAR_INSTRUCTIONS_PUBKEY,
+  Transaction,
+} from "@solana/web3.js";
+import { BN } from "bn.js";
+import { randomBytes } from "crypto";
 
 describe("anchor-dice-game-q4-25", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.anchorDiceGameQ425 as Program<AnchorDiceGameQ425>;
+  const program = anchor.workspace.anchorDice2026 as Program<AnchorDice2026>;
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  let house = new Keypair();
+  let player = new Keypair();
+  let seed = new BN(randomBytes(16));
+
+  let [vault, vaultBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), house.publicKey.toBuffer()],
+    program.programId,
+  );
+
+  let [bet, betBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("bet"), vault.toBuffer(), seed.toBuffer("le", 16)],
+    program.programId,
+  );
+  let signature: Uint8Array;
+
+  it("Airdrop", async () => {
+    // Airdrop sol to house and player
+    await Promise.all(
+      [house, player].map(async (key) => {
+        return await provider.connection.requestAirdrop(
+          key.publicKey,
+          1000 * LAMPORTS_PER_SOL,
+        );
+      }),
+    );
+  });
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    // Intialize house and vault
+    const tx = await program.methods
+      .initialize(new BN(LAMPORTS_PER_SOL).mul(new BN(100)))
+      .accountsStrict({
+        house: house.publicKey,
+        vault,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([house])
+      .rpc()
+      .then(confirmTx);
     console.log("Your transaction signature", tx);
   });
+
+  it("Place a bet", async () => {
+    let tx = await program.methods
+      .placeBet(seed, 40, new BN(LAMPORTS_PER_SOL / 100))
+      .accountsStrict({
+        player: player.publicKey,
+        house: house.publicKey,
+        vault,
+        bet,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([player])
+      .rpc()
+      .then(confirmTx);
+
+    console.log("Bet placed: ", tx);
+  });
+
+  it("Resolves bet", async () => {
+    let account = await provider.connection.getAccountInfo(bet, "confirmed");
+    let sig_ix = Ed25519Program.createInstructionWithPrivateKey({
+      privateKey: house.secretKey,
+      message: account.data.subarray(8),
+    });
+
+    const resolve_ix = await program.methods
+      .resolveBet(Buffer.from(sig_ix.data.buffer.slice(16 + 32, 16 + 32 + 64)))
+      .accountsStrict({
+        player: player.publicKey,
+        house: house.publicKey,
+        vault,
+        bet,
+        instructionSysvar: SYSVAR_INSTRUCTIONS_PUBKEY,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([house])
+      .instruction();
+
+    const tx = new Transaction().add(sig_ix).add(resolve_ix);
+
+    try {
+      await sendAndConfirmTransaction(program.provider.connection, tx, [house]);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  });
 });
+
+const confirmTx = async (signature: string): Promise<string> => {
+  const latestBlockHash = await anchor
+    .getProvider()
+    .connection.getLatestBlockhash();
+  await anchor
+    .getProvider()
+    .connection.confirmTransaction(
+      { signature, ...latestBlockHash },
+      "confirmed",
+    );
+
+  return signature;
+};
